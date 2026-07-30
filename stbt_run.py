LewisHaley/stb-tester

commit 7100a290a4e52cd8ec65c185974658c7c1a77477

pyproject: Declare all dependencies (runtime and dev)
This commit declares all runtime and development dependencies in
`pyproject.toml`, which is standard practice for Python packaging. It
leverages the `dependency-groups` option [1][2] to declare dev dependencies
in isolation to the core and extra dependencies, meaning that installers of
the package don't need to pull in, e.g. `responses` which is only used in
stb-tester's unit tests and not its production code.

All unnecessary pins have been removed (deemed necessary or not by whether
CI continue to pass). Note that because `pylint` is a core dependency as
well as a dev dependency, its pin has had to be lowered by a minor version
in order to keep the current lint status of the project.

`deptry` [3] was used to identify the complete set of *primary*
dependencies. Its options are committed to `pyproject` although no
Makefile entry or CI reference is added.

All this means that the CI Dockerfile does not need to install Python
packages via the system package manager. Instead, all[*] dependencies (with
the exception of "cec", see below) are installed via `pip` during the
runtime of the CI container. In theory this could be moved into the CI
image, but see commit:
  ".github->ubuntu2204: Build docker ci image on the fly"

The `requirements.txt` file is the new source-of-truth for the absolute
dependency pins used in CI (*not* install or runtime!). This is generated
using `uv` [4] as "pip-tools" (a "pip" extension package) does not yet
support "dependency-groups", mentioned above. See `depedency-management.md`
for an in-depth explanation.

[*] The one exception is the "cec" module provided by `python3-cec`. This
is *not* the same package as `cec` from PyPi [4]. `python3-cec` are Python
bindings vendors directly by the "Pulse-Eight" - the group that maintains
the underlying `libcec` library. See [5].

[1] https://peps.python.org/pep-0735/
[2] https://packaging.python.org/en/latest/specifications/dependency-groups/

.github/workflows/python-package.yml
@@ -17,9 +17,16 @@ jobs:
     - uses: actions/checkout@v3
     - name: install & test
       run: |
+        # We need libgirepository1.0-dev to install PyGObject, as we don't have a wheel available.
+        # In the venv, we install our package, which doesn't have pytest as a runtime dependency,
+        # so we install it separately.
+        # The source package files are then removed to prove that we are testing the installed
+        # package, not the source files.
+        sudo apt-get update &&
+        sudo apt-get install libgirepository1.0-dev &&
         python3 -m venv build_env &&
         source build_env/bin/activate &&
-        pip3 install pytest==6.2.5 &&
+        pip3 install pytest &&
         pip3 install . &&
         rm -rf stbt_core _stbt &&
         make check-pythonpackage PYTEST=pytest

pyproject.toml
@@ -15,10 +15,14 @@ classifiers = [
 ]
 requires-python = ">=3.10"
 dependencies = [
-    "astroid==2.11.6",
-    "attrs==21.2.0",
-    "opencv-python-headless~=4.5",
-    "pylint==2.14.3",
+    "astroid",
+    "attrs",
+    "lmdb",
+    "numpy",
+    "opencv-python-headless",
+    "pylint<2.13",
+    "PyGObject<3.51",  # <3.51 because of https://pypi.org/project/PyGObject/3.51.0.dev0/
+    "xxhash",
 ]
 
 [project.urls]
@@ -26,13 +30,45 @@ dependencies = [
 
 [project.optional-dependencies]
 ocr = [
-    "lxml==4.8.0",
+    "lxml",
+]
+aten_pe6108g = [
+    "pysnmp<5",
+    "pyasn1<0.6.1",  # Required due to a bug in pysnmp's packaging, same below
+]
+apc7xxx = [
+    "pysnmp<5",
+    "pyasn1<0.6.1",
+]
+aviosys_8800_pro = [
+    "pyserial",
 ]
 debug = [
-    "Jinja2==3.0.3",
+    "Jinja2",
+    "IPython<8",
+    "markupsafe"
 ]
 keyboard = [
-    "networkx==2.4",
+    "networkx",
+]
+roku = [
+    "requests",
+]
+
+[dependency-groups]
+deps = [
+    "deptry",
+]
+type = [
+    "pyright[nodejs]",
+]
+lint = [
+    "pycodestyle",
+    "pytest",
+]
+test = [
+    "pytest",
+    "responses",
 ]
 
 [build-system]
@@ -211,4 +247,42 @@ ignore = [
     "tests/test_ocr.py",
     "tests/test_stbt_debug.py",
     "tests/test_transition.py",
+]
+
+[tool.deptry]
+known_first_party = [
+    "stbt_core",
+    "_stbt",
+]
+exclude = [
+    "extra/nbstripout.py",
+    "tests/validate-ocr.py",
+]
+
+[tool.deptry.package_module_name_map]
+attrs = "attr"
+IPython = "IPython"
+PyGObject = "gi"
+pyserial = "serial"
+opencv-python-headless = "cv2"
+
+[tool.deptry.per_rule_ignores]
+# DEP001: imported but not declared as a dependency
+DEP001 = [
+    # cec is provide only by `python3-cec` in Ubuntu, so it's a dependency listed in this file.
+    "cec",
+    # "numba" is only used in a performance test of `sqdiff.py`
+    "numba",
+]
+# DEP002: declared as a dependency but not imported
+DEP002 = [
+    # "pyasn1" is a dependency of `pysnmp`, which has a bug in its packaging that requires us to explicitly depend on it.
+    "pyasn1",
+]
+# DEP004: declared as a dev dependency but imported in production code
+# These *are* dev dependencies, but the tests they are used for are in the
+# production modules.
+DEP004 = [
+    "pytest",
+    "responses",
 ]

.github/workflows/ubuntu2204.dockerfile
@@ -20,32 +20,15 @@ RUN export DEBIAN_FRONTEND=noninteractive && \
         gstreamer1.0-x \
         gzip \
         language-pack-en \
+        libcairo2-dev \
+        libgirepository1.0-dev \
         librsvg2-bin \
         lirc \
         moreutils \
         parallel \
-        pep8 \
-        pylint \
         python3-cec \
         python3-dev \
-        python3-docutils \
-        python3-gi \
-        python3-ipython \
-        python3-jinja2 \
-        python3-lmdb \
-        python3-lxml \
-        python3-networkx \
-        python3-nose \
-        python3-numpy \
-        python3-opencv \
         python3-pip \
-        python3-pysnmp4 \
-        python3-pytest \
-        python3-requests \
-        python3-responses \
-        python3-serial \
-        python3-xxhash \
-        python3-yaml \
         ssh \
         sudo \
         tar \
@@ -56,7 +39,9 @@ RUN export DEBIAN_FRONTEND=noninteractive && \
         xterm && \
     apt-get clean
 
-RUN pip3 install pyright[nodejs] && pyright --version
+# Update pip to latest. The version install by the Ubuntu system package is old
+# and can't build the pycairo wheel.
+RUN pip3 install --no-cache-dir --upgrade pip
 
 RUN mkdir -p $HOME/.parallel && \
     touch $HOME/.parallel/will-cite  # Silence citation warning

.github/workflows/ubuntu2204.yml
@@ -26,6 +26,7 @@ jobs:
           cat \
         && docker container start stb-tester-ci
     - run: docker container exec stb-tester-ci git config --global --add safe.directory /project
+    - run: docker container exec stb-tester-ci pip install -r requirements.txt
     - run: docker container exec stb-tester-ci tesseract --version
     - run: docker container exec stb-tester-ci make check-pylint
     - run: docker container exec stb-tester-ci pyright --version

tests/test-pytest.sh
@@ -1,7 +1,7 @@
 # Run with ./run-tests.sh
 
 _pytest() {
-    PYTHONPATH=$srcdir:$PYTHONPATH pytest-3 -p stbt_run "$@"
+    PYTHONPATH=$srcdir:$PYTHONPATH python3 -m pytest -p stbt_run "$@"
 }
 
 test_pytest() {

commit ff2f67bc20162f80ebee1c55ba98d53611e1a18f

Restore Stb-tester Node package support
Clients of the Stb-tester Node need to be able to install the stb-tester
package so that it can provide IDE integration (lint and intellisense),
but without it requiring the runtime dependencies which are Linux (Unix)
specific such as PyGObject. In addition, the dependencies need to match
those installed in the Stb-tester Node so that the linting matches client
and server side.

This support was removed/broken in commit 7100a290, which declared and
unpinned the previous dependencies that existed in the former `setup.py`,
and is re-established here. The package now has zero dependencies by
default. Instead, users should install either `stbt_core[stb-tester-node]`
or `stbt_core[runtime]` (with any other extras as required). The former
contains the former list of IDE dependencies, with 2 changes:
1. opencv-python~=4.5 -> opencv-python-headless~=4.5 as per 15970cb6
2. xxhash is added as per fc006d4

The "Python Package" workflow removes the install of `libgirepository`,
which Stb-tester Node clients may not have, and installs specifically the
`stbt_core[stb-tester-node]` dependency set, thus restoring previous
behaviour.

One knnock-on effect here is that, in order for `pylint` to be pinned in
the `stb-tester-node` group, the previous constraint of `pylint<2.13`
cannot be maintained as they are incompatible. Thus, the dependencies files
(uv.lock and requirements.txt) show pin changes for `pylint` (et al.) and
`attrs`. The `pylint` change requires additional disables in `stbt_run.py`.

.github/workflows/python-package.yml
@@ -17,16 +17,9 @@ jobs:
     - uses: actions/checkout@v3
     - name: install & test
       run: |
-        # We need libgirepository1.0-dev to install PyGObject, as we don't have a wheel available.
-        # In the venv, we install our package, which doesn't have pytest as a runtime dependency,
-        # so we install it separately.
-        # The source package files are then removed to prove that we are testing the installed
-        # package, not the source files.
-        sudo apt-get update &&
-        sudo apt-get install libgirepository1.0-dev &&
         python3 -m venv build_env &&
         source build_env/bin/activate &&
         pip3 install pytest &&
-        pip3 install . &&
+        pip3 install .[stb-tester-node] &&
         rm -rf stbt_core _stbt &&
         make check-pythonpackage PYTEST=pytest

pyproject.toml
@@ -15,20 +15,27 @@ classifiers = [
 ]
 requires-python = ">=3.10"
 dependencies = [
-    "astroid",
-    "attrs",
-    "lmdb",
-    "numpy",
-    "opencv-python-headless",
-    "pylint<2.13",
-    "PyGObject<3.51",  # <3.51 because of https://pypi.org/project/PyGObject/3.51.0.dev0/
-    "xxhash",
+    # Intentionally blank. There are 2 client types for this package:
+    # 1. Clients of the stb-tester.com Node. The clients don't run the package
+    #    themselves, but install the package in their dev environment. Such
+    #    clients should install, at a minimum, the `stb_tester_node` optional
+    #    dependency.
+    # 2. Clients that run the package themselves. Such clients should install,
+    #    at a minimum, the `runtime` optional dependency.
 ]
 
 [project.urls]
 "Home Page" = "https://stb-tester.com"
 
 [project.optional-dependencies]
+runtime = [
+    "attrs",
+    "lmdb",
+    "numpy",
+    "opencv-python-headless",
+    "PyGObject<3.51",  # <3.51 because of https://pypi.org/project/PyGObject/3.51.0.dev0/
+    "xxhash",
+]
 ocr = [
     "lxml",
 ]
@@ -54,6 +61,14 @@ keyboard = [
 roku = [
     "requests",
 ]
+stb_tester_node = [
+    # These are precise pins needed to be 100% compatible with the stb-tester.com Node.
+    "astroid==2.11.6",
+    "attrs==21.2.0",
+    "opencv-python-headless~=4.5",
+    "pylint==2.14.3",
+    "xxhash",
+]
 
 [dependency-groups]
 deps = [
@@ -64,6 +79,7 @@ type = [
 ]
 lint = [
     "pycodestyle",
+    "pylint",
     "pytest",
 ]
 test = [

stbt_run.py
@@ -97,9 +97,9 @@ def pytest_sessionstart(session):
     dut = _stbt.core.new_device_under_test_from_config(args)
     session.dut = dut
     session.video = video(args, dut)
-    session.video.__enter__()
+    session.video.__enter__()  # pylint: disable=unnecessary-dunder-call
     session.imgproc_cache = imgproc_cache.setup_cache(filename=args.cache)
-    session.imgproc_cache.__enter__()
+    session.imgproc_cache.__enter__()  # pylint: disable=unnecessary-dunder-call
     dut.get_frame()  # wait until pipeline is rolling
 
 